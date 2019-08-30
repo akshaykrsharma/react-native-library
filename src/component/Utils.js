329114b28fa32f8ac7aad407b2fe70697dd444fe
Akshay Sharma <sharma.akshay5@gmail.com>
2019-08-30 10:35:07 +0530

    Removed Async storage

--- src/component/Utils.js
+++ src/component/Utils.js
@@ -1,6 +1,5 @@
 import {Dimensions, Platform} from 'react-native';
 const {height} = Dimensions.get('window');
-import AsyncStorage from '@react-native-community/async-storage';
 
 export default class Utils {
   static styleMerger(...styles) {
@@ -19,25 +18,25 @@ export default class Utils {
     return typeof comp == 'object';
   }
 
-  static _retrieveData = async (key, cb) => {
-    try {
-      const value = await AsyncStorage.getItem(key);
-      if (value !== null) {
-        // We have data!!
-        cb(value);
-      } else {
-        cb(null);
-      }
-    } catch (error) {
-      // Error retrieving data
-    }
-  };
+  // static _retrieveData = async (key, cb) => {
+  //   try {
+  //     const value = await AsyncStorage.getItem(key);
+  //     if (value !== null) {
+  //       // We have data!!
+  //       cb(value);
+  //     } else {
+  //       cb(null);
+  //     }
+  //   } catch (error) {
+  //     // Error retrieving data
+  //   }
+  // };
 
-  static _storeData = async (key, value) => {
-    try {
-      await AsyncStorage.setItem(key, value);
-    } catch (error) {
-      // Error saving data
-    }
-  };
+  // static _storeData = async (key, value) => {
+  //   try {
+  //     await AsyncStorage.setItem(key, value);
+  //   } catch (error) {
+  //     // Error saving data
+  //   }
+  // };
 }
